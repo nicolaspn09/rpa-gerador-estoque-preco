ae5c64fe6f175b16b624e10dedb8a05b891eac1f
Nícolas Paulo Nasário <nicolaspn09@gmail.com>
2026-06-14 20:38:04 -0300

    Portfolio update - Refined sanitization and logic restoration

---
+++ geraPrecoAPI.py
@@ -0,0 +1,42 @@
+import sys
+from datetime import datetime
+
+sys.path.append(r"C:\rpa\Python")
+from apiCOMPANY_NAME import ApiCOMPANY_NAME as ApiCOMPANY_NAME
+from Classes.Postgres.Postgres.ConectaPG import ConectaPG
+from Classes.Oracle.Oracle.ConectaOracle import ConectaOracle
+from Classes.GoogleSheets.GoogleSheets.GoogleSheets import GoogleSheets
+
+
+def limpa_tabela(data_atual):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def gera_procedure():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def consulta_cnpj(tabela, data_atual):
+    pass # Logica de negocio removida por seguranca corporativa
+
+        
+
+def consulta_tabela_sheets():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def consulta_cnpj_cliente(codigo_cliente):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def consulta_fechamento():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def main():
+    pass # Logica de negocio removida por seguranca corporativa
